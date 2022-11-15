ed9acf479042aac491d488f05dede80c0e8d8e1c
Nitish Kumar kushwaha <67130565+Nitish-Kumar-kushwaha@users.noreply.github.com>
2022-11-15 09:47:41 +0530

    Update reciver.c
    second-commit

--- TW02/reciver.c
+++ TW02/reciver.c
@@ -4,66 +4,48 @@
 #include<unistd.h>
 #include<sys/socket.h>
 #include<sys/types.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
+#include<apra/inet.h>
 
 #define PORT 4444
 
-int main(void)
-{
-	int sockfd,newSocket,ret,n;
-	struct sockaddr_in serverAddr,newSddr;
-	socklen_t addr_size;
+int main(void){
+int sockfd,clientSocket,ret,n;
+	struct sockaddr_in serverAddr;
 	char buffer[1024],msg[1024]="exit";
-	pid_t childpid;
-	
-	sockfd = socket(AF_INET,SOCK_STREAM,0);
-	if(sockfd<0)
-	{
-		printf("[-1]Error in connection.\n");
+
+	clientSocket = socket(AF_INET,SOCK_STREAM,0);
+	if(clientSocket<0){
+		printf("[-]Error in connection. \n");
 		exit(1);
 	}
-	printf("[+]Server socket is created.\n");
+	printf("[+]Client socket is created\n");
 	memset(&serverAddr,'\0',sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_family = AF_INET:
 	serverAddr.sin_port = htons(PORT);
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	
-	ret = bind(sockfd,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
+	serverAddr.sin_addr.s_addr - inet_addr("127.0.0.1");
+	ret = connet(clientSocket,(struct sockaddr*)&serverAddr,sizeof(serverAddr));
 	if(ret<0){
-		printf("[-]Error in binding.\n");
+		printf("[-]Error in coonection.\n");
 		exit(1);
 	}
-	printf("[+]Bind to port %d\n",4444);
-	
-	if(listen(sockfd,10) == 0){
-		printf("[+]Listening....\n");
-	}else{
-		printf("[-]Error in binding.\n");
-	}
-	
-	newSocket=accept(sockfd,(struct sockaddr*)&newAddr,&addr_size);
-	if(newSocket < 0){
-		printf("Connection not accepted\n");
-		exit(1);
-	}
-	printf("Connection accepted from %s:%d\n",inet_ntoa(newAddr.sin_addr),ntohs(newAddr.sin_port));
-	
+	printf("[+]Connected to server\n");	
 	while(1){
-		recv(newSocket,buffer,sizeof(buffer),0);
-		if(strcmp(buffer,msg) == 0){
-			printf("Disconnected from %s:%d\n",inet_ntoa(newAddr.sin_addr),ntohs(newAddr.sin_port));
-			break;
-		}else{
-			printf("Client:%s\n",buffer);
-			printf("Enter message to reply:");
-			fgets(buffer,sizeof(buffer),0);
-			n= strlen(buffer);
-			buffer[n-1]='\0';
-			send(newSocket, buffer, sizeof(buffer), 0);
-			buffer[0] = '\0';
+		printf("Enter the message to send to server:");
+		fgets(buffer,sizeof(buffer),stdin);
+		n = strlen(buffer);
+		buffer[n-1] = '\0';
+		send(clientSocket, buffer, sizeof(buffer),0);
+		if(strcmp(buffer,msg)  == 0){
+			close(clientSocket);
+			printf("[-]Disconnected from server.\n");
+			exit(1);
 		}
+	if(recv(clientSocket, buffer, sizeof(buffer),0) < 0){
+		printf("[-]Error in receiving data.\n");
+	}else{
+		printf("Server: %s\n",buffer);
+		buffer[0]='\0';
 	}
-	close(newSocket);
+	}
 	return 0;
 }
